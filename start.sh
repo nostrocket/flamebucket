202c877af9f0e7af7c900385aa7ed8181499c84e
gsovereignty <gsovereignty@proton.me>
2023-08-23 14:25:46 +0800

    problem: sometimes not enough time to open port

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ export PATH="$PATH:$HOME/.local/bin" #if manually installed protobuf
 tmux new-session -d bash
 tmux split-window -h bash
 tmux send -t 0:0.0 "./start_manager.sh" C-m
+sleep 1
 tmux send -t 0:0.1 "./start_relay.sh" C-m
 tmux -2 attach-session -d
